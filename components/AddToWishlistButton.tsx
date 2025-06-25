2c67a26daa4684deecade7d85608d92e00f8548d
vulture89 <pansilukv@gmai.com>
2025-06-25 14:59:42 +0530

    Favbtn added

--- components/AddToWishlistButton.tsx
+++ components/AddToWishlistButton.tsx
@@ -1,18 +1,48 @@
+"use client";
 import { cn } from "@/lib/utils";
 import { Product } from "@/sanity.types";
+import useStore from "@/store";
 import { Heart } from "lucide-react";
-import React from "react";
+import React, { useEffect, useState } from "react";
+import toast from "react-hot-toast";
 
 const AddToWishlistButton = ({
   product,
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
   className,
 }: {
   product: Product;
   className?: string;
 }) => {
+  const { favoriteProduct, addToFavorite } = useStore();
+  const [existingProduct, setExistingProduct] = useState<Product | null>(null);
+
+  useEffect(() => {
+    const availableProduct = favoriteProduct?.find(
+      (item) => item?._id === product?._id
+    );
+    setExistingProduct(availableProduct || null);
+  }, [product, favoriteProduct]);
+
+  const handleFavorite = (e: React.MouseEvent<HTMLSpanElement>) => {
+    e.preventDefault();
+    if (product?._id) {
+      addToFavorite(product).then(() => {
+        toast.success(
+          existingProduct
+            ? "Product removed successfully!"
+            : "Product added successfully!"
+        );
+      });
+    }
+  };
+
   return (
     <div className={cn("absolute top-2 right-2 z-10")}>
-      <button className="p-2.5 rounded-full hover:bg-shop_dark_green hover:text-white bg-deal-bg hoverEffect">
+      <button
+        onClick={handleFavorite}
+        className={`p-2.5 rounded-full hover:bg-shop_dark_green/80 hover:text-white hoverEffect  ${existingProduct ? "bg-shop_dark_green/80 text-white" : "bg-lightColor/10"}`}
+      >
         <Heart size={15} />
       </button>
     </div>
